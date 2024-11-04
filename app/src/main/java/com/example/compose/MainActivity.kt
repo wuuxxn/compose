efcebdebbdb7fdf5785432d37be9755448f3f3cc
wuuxxn <owen20050723@gmail.com>
2024-11-04 15:49:10 +0800

    v5_資料庫按鈕

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -52,6 +52,8 @@ fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("吳昕恩")}
     var userWeight by remember { mutableStateOf(3800)}
     var userPassword by remember { mutableStateOf("")}
+    var msg by remember { mutableStateOf("訊息")}
+
 
 
     Column {
@@ -91,6 +93,19 @@ fun Birth(m: Modifier){
 
 
         Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"+ "\n密碼：$userPassword")
+        Row {
+            Button(onClick = {  }) {
+                Text("新增/修改資料")
+            }
+            Button(onClick = {  }) {
+                Text("查詢資料")
+            }
+            Button(onClick = {  }) {
+                Text("刪除資料")
+            }
+        }
+        Text(text = msg)
+
     }
 
 

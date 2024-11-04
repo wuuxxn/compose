c8aa0f1110ed78d62dac8b804ec54dae92343e75
wuuxxn <owen20050723@gmail.com>
2024-11-04 15:33:47 +0800

    v4_密碼輸入

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -51,6 +51,8 @@ class MainActivity : ComponentActivity() {
 fun Birth(m: Modifier){
     var userName by remember { mutableStateOf("吳昕恩")}
     var userWeight by remember { mutableStateOf(3800)}
+    var userPassword by remember { mutableStateOf("")}
+
 
     Column {
         TextField(
@@ -75,9 +77,20 @@ fun Birth(m: Modifier){
             keyboardOptions = KeyboardOptions
                 (keyboardType = KeyboardType.Number)
         )
+        TextField(
+            value = userPassword,
+            onValueChange = { newText ->
+                userPassword = newText
+            },
+            label = { Text("密碼") },
+            placeholder = { Text(text = "請輸入您的密碼") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions
+                (keyboardType = KeyboardType.Password)
+        )
 
 
-        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克")
+        Text("您輸入的姓名是：$userName\n出生體重為：$userWeight 公克"+ "\n密碼：$userPassword")
     }
 
 
